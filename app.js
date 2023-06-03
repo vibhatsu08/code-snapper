f94e9e08b626637fa32d84801c2fc44fd2910ed3
vibhatsu08 <mistryvedant17@gmail.com>
2023-06-04 04:20:21 +0530

    V1.0 of the JS file for the project.

---
+++ app.js
@@ -0,0 +1,122 @@
+let finalTagCounts = {}
+
+let codeInput = document.getElementById('code-input');
+let tagsArea = document.getElementById('tags-area');
+
+document.getElementById('snip-it-button').addEventListener('click', function() {
+    let code = codeInput.value.trim();
+    if (code === '') {
+		codeInput.value = "";
+    	tagsArea.value = "";
+        return;
+    }
+
+	if (tagsArea.value.trim() == "" ) {
+		codeInput.value = "";
+    	tagsArea.value = "";
+		return;
+	}
+    let tags = extractTags(tagsArea.value.trim());
+    console.log(tags)
+    let snippetDiv = createSnippetDiv(code, tags);
+    let snippetsBody = document.getElementById('snippets-body');
+    snippetsBody.appendChild(snippetDiv);
+    codeInput.value = "";
+    tagsArea.value = "";
+
+    finalTagsCount();
+    updateTagCounts(tags, 1);
+});
+
+
+function extractTags(tagsText) {
+    let tags = tagsText.trim().split(/\s+/);
+    console.log(Array.from(new Set(tags)))
+    return Array.from(new Set(tags));
+}
+
+function createSnippetDiv(code, tags) {
+    let snippetDiv = document.createElement('div');
+    let snippetButtonsDiv = document.createElement('div');
+    let closeButtonDiv = document.createElement('button');
+    let enlargeButtonDiv = document.createElement('button');
+    let gptButtonsContainerDiv = document.createElement('div');
+    let useCaseButton = document.createElement('button');
+    let explainButton = document.createElement('button');
+    let translateButton = document.createElement('button');
+    let codeBodyDiv = document.createElement('div');
+    let codeGroundDiv = document.createElement('div');
+    let codeTagsDiv = document.createElement('div');
+
+    codeGroundDiv.textContent = code;
+    useCaseButton.textContent = 'use case';
+    explainButton.textContent = 'explain';
+    translateButton.textContent = 'translate';
+
+    snippetDiv.classList.add('snippet');
+    snippetButtonsDiv.classList.add('snippet-buttons');
+    closeButtonDiv.classList.add('close-button');
+    enlargeButtonDiv.classList.add('enlarge-button');
+    gptButtonsContainerDiv.classList.add('gpt-buttons');
+    useCaseButton.classList.add('gpt-use-case');
+    explainButton.classList.add('gpt-explain');
+    translateButton.classList.add('gpt-translate');
+    codeBodyDiv.classList.add('code-body');
+    codeGroundDiv.classList.add('code-ground');
+    codeTagsDiv.classList.add('code-tags');
+
+    closeButtonDiv.addEventListener('click', function() {
+        deleteSnippet(snippetDiv);
+    });
+
+    snippetButtonsDiv.appendChild(closeButtonDiv);
+    snippetButtonsDiv.appendChild(enlargeButtonDiv);
+    snippetDiv.appendChild(snippetButtonsDiv);
+    snippetDiv.appendChild(codeBodyDiv);
+    snippetDiv.appendChild(gptButtonsContainerDiv);
+    gptButtonsContainerDiv.appendChild(useCaseButton);
+    gptButtonsContainerDiv.appendChild(explainButton);
+    gptButtonsContainerDiv.appendChild(translateButton);
+    codeBodyDiv.appendChild(codeGroundDiv);
+    codeBodyDiv.appendChild(codeTagsDiv);
+    codeBodyDiv.appendChild(gptButtonsContainerDiv);
+
+    tags.forEach(function(tag) {
+        let tagSpan = document.createElement('span');
+        tagSpan.classList.add('tags');
+        tagSpan.textContent = tag;
+        codeTagsDiv.appendChild(tagSpan);
+    
+        updateTagCounts([tag], 1);
+    });
+    return snippetDiv;
+}
+function deleteSnippet(snippet) {
+    let snippetsBody = document.getElementById('snippets-body');
+    snippetsBody.removeChild(snippet);
+}
+function finalTagsCount() {
+    let finalTagsBodyDiv = document.getElementById('final-tags-body');
+    finalTagsBodyDiv.innerHTML = '';
+
+    for (let tag in finalTagCounts) {
+        let finalTagCountsSpan = document.createElement('div');
+        let finalTagCountsSpanTag = document.createElement("span");
+        let finalTagCountsSpanTagCount = document.createElement("span");
+        finalTagCountsSpan.classList.add('tags');
+        finalTagCountsSpanTag.textContent = tag;
+        finalTagCountsSpanTagCount.textContent = finalTagCounts[tag]
+        finalTagCountsSpan.appendChild(finalTagCountsSpanTag);
+        finalTagCountsSpan.appendChild(finalTagCountsSpanTagCount);
+        finalTagsBodyDiv.appendChild(finalTagCountsSpan);
+    }
+}
+function updateTagCounts(tags, increment) {
+    tags.forEach(function(tag) {
+        if (finalTagCounts[tag]) {
+            finalTagCounts[tag] += increment;
+        } else {
+            finalTagCounts[tag] = increment;
+        }
+    });
+}
